32fb648c7db7e654b7c42871266d919b0516e54b
tomo-local <shinsi4545@gmail.com>
2024-06-08 15:40:50 +0900

    fix

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,24 +2,24 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $_404 from "./src/routes/_404.tsx";
-import * as $_app from "./src/routes/_app.tsx";
-import * as $api_joke from "./src/routes/api/joke.ts";
-import * as $greet_name_ from "./src/routes/greet/[name].tsx";
-import * as $index from "./src/routes/index.tsx";
-import * as $Counter from "./src/islands/Counter.tsx";
+import * as $_404 from "./routes/_404.tsx";
+import * as $_app from "./routes/_app.tsx";
+import * as $api_joke from "./routes/api/joke.ts";
+import * as $greet_name_ from "./routes/greet/[name].tsx";
+import * as $index from "./routes/index.tsx";
+import * as $Counter from "./islands/Counter.tsx";
 import { type Manifest } from "$fresh/server.ts";
 
 const manifest = {
   routes: {
-    "./src/routes/_404.tsx": $_404,
-    "./src/routes/_app.tsx": $_app,
-    "./src/routes/api/joke.ts": $api_joke,
-    "./src/routes/greet/[name].tsx": $greet_name_,
-    "./src/routes/index.tsx": $index,
+    "./routes/_404.tsx": $_404,
+    "./routes/_app.tsx": $_app,
+    "./routes/api/joke.ts": $api_joke,
+    "./routes/greet/[name].tsx": $greet_name_,
+    "./routes/index.tsx": $index,
   },
   islands: {
-    "./src/islands/Counter.tsx": $Counter,
+    "./islands/Counter.tsx": $Counter,
   },
   baseUrl: import.meta.url,
 } satisfies Manifest;
