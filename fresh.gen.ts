d3fa76c50eaa9fc7e5bff37811fb62dd768fae6b
tomo-local <shinsi4545@gmail.com>
2024-06-08 15:36:35 +0900

    push

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,12 +2,12 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $_404 from "./routes/_404.tsx";
-import * as $_app from "./routes/_app.tsx";
-import * as $api_joke from "./routes/api/joke.ts";
-import * as $greet_name_ from "./routes/greet/[name].tsx";
-import * as $index from "./routes/index.tsx";
-import * as $Counter from "./islands/Counter.tsx";
+import * as $_404 from "./src/routes/_404.tsx";
+import * as $_app from "./src/routes/_app.tsx";
+import * as $api_joke from "./src/routes/api/joke.ts";
+import * as $greet_name_ from "./src/routes/greet/[name].tsx";
+import * as $index from "./src/routes/index.tsx";
+import * as $Counter from "./src/islands/Counter.tsx";
 import { type Manifest } from "$fresh/server.ts";
 
 const manifest = {
